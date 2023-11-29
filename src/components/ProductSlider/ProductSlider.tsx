a45c6da1da64b10bd2bbfbead012f7ef59a97356
Yevhenii <zheki2004@gmail.com>
2023-11-30 00:27:41 +0200

    implement dots to product slider

--- src/components/ProductSlider/ProductSlider.tsx
+++ src/components/ProductSlider/ProductSlider.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 import { ProductCard } from "../ProductCard/ProductCard";
 import './ProductSlider.scss';
 import WestIcon from '@mui/icons-material/West';
 import EastIcon from '@mui/icons-material/East';
 import { products } from "../../MockProducts";
+import cn from 'classnames';
 
 type Props = {
   title: string,
@@ -11,14 +12,14 @@ type Props = {
 
 const mobile = window.innerWidth <= 1024;
 
-export const ProductSlider: React.FC<Props> = ({ title }) => {
+export const ProductSlider: React.FC<Props> = memo(({ title }) => {
   const [slideIndex, setSlideIndex] = useState(0);
 
-  const handlePrevSlideMobile = () => {
+  const handlePrevSlide = () => {
     setSlideIndex((prevIndex) => Math.max(prevIndex - 2, 0));
   };
 
-  const handleNextSlideMobile = () => {
+  const handleNextSlide = () => {
     setSlideIndex((prevIndex) => Math.min(prevIndex + 2, products.length - 2));
   };
 
@@ -31,20 +32,37 @@ export const ProductSlider: React.FC<Props> = ({ title }) => {
       <div className="slider-top">
         <h2 className="slider-top__title">{title}</h2>
         <div className="slider-top__content">
-          <button 
-            onClick={handlePrevSlideMobile}
+          {mobile ? (
+            <div className="slider-top__dots">
+            {Array.from({ length: Math.ceil(products.length / 2) }).map((_, ind) => (
+              <div
+                key={ind}
+                onClick={() => setSlideIndex(ind * 2)}
+                className={cn('slider-top__dot', {
+                  active: ind * 2 === slideIndex,
+                })}
+              />
+            ))}
+          </div>
+          ) : (
+            <>
+            <button
+            onClick={handlePrevSlide}
             className="slider-top__button"
             disabled={prevButtonDisabled}
           >
             <WestIcon />
           </button>
-          <button 
-            onClick={handleNextSlideMobile}
+          <button
+            onClick={handleNextSlide}
             disabled={nextButtonDisabled}
             className="slider-top__button"
           >
             <EastIcon />
           </button>
+            </>
+          )}
+          
         </div>
       </div>
       <div className="productSlider">
@@ -58,4 +76,4 @@ export const ProductSlider: React.FC<Props> = ({ title }) => {
       </div>
     </>
   );
-};
+});
