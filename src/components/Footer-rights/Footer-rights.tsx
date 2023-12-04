469fbc4d17d3120ea9f2fce0288a4fa28ca8aabb
Yevhenii <zheki2004@gmail.com>
2023-12-04 13:54:39 +0200

    implemented whole footer

---
+++ src/components/Footer-rights/Footer-rights.tsx
@@ -0,0 +1,10 @@
+import './Footer-right.scss';
+
+export const FooterRights = () => {
+  return (
+    <div className="right">
+      <div className="right__up">MAKEUP™. BEAUTY WITHOUT LIMITS</div>
+      <div className="right__down">© MAKEUP 2009-2023</div>
+    </div>
+  )
+};
